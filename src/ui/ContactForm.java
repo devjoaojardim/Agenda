fbd4741a8a5e2a79e5fc579dba789ac116841b94
vitinho <victorsilva428@gmail.com>
2021-12-28 12:53:33 -0300

    contactForm apply

---
+++ src/ui/ContactForm.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+
+public class ContactForm extends JFrame {
+
+    private JPanel rootPanel;
+
+    public ContactForm() {
+        setContentPane(rootPanel);
+        setSize(500, 250);
+        setVisible(true);
+    }
+}
