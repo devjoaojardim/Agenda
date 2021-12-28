4787c55ec501f45511afd772f0a522106d1e50bb
vitinho <victorsilva428@gmail.com>
2021-12-28 13:24:43 -0300

    Evento primario dos buttons

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
@@ -14,5 +17,27 @@ public class ContactForm extends JFrame {
         setContentPane(rootPanel);
         setSize(500, 250);
         setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        buttonCancele.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
+
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //coloca depois
+            }
+        });
     }
 }
--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
@@ -9,15 +11,31 @@ public class MainForm extends JFrame {
     private JButton buttonRemove;
     private JTable tableContacts;
 
-     public MainForm() {
-         setContentPane(rootPanel);
-         setSize(500, 250);
-         setVisible(true);
-         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2 );
+    public MainForm() {
+        setContentPane(rootPanel);
+        setSize(500, 250);
+        setVisible(true);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setListeners();
+    }
 
-         //setListeners();
-     }
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();
+            }
+        });
+
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
