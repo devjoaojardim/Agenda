f85674fd52ccabff5fffe2f6d79c5da9f8c9b2a3
vitinho <victorsilva428@gmail.com>
2021-12-29 19:18:07 -0300

    Class para salvar o contato

--- src/Start.kt
+++ src/Start.kt
@@ -3,3 +3,4 @@ import ui.MainForm
 fun main() {
     MainForm()
 }
+
---
+++ src/business/ContactBusiness.kt
@@ -0,0 +1,29 @@
+package business
+
+class ContactBusiness {
+
+    fun validate(name: String, phone: String){
+        if (name == ""){
+            throw Exception("Nome é obrigatório!")
+        }
+        if (phone == ""){
+            throw Exception("Telefone é obrigatório!")
+        }
+    }
+    fun validateDelete(name: String, phone: String){
+        if (name == "" || phone == ""){
+            throw Exception("É necessario selecionar um contato antes de remover")
+        }
+    }
+
+    fun save(name: String, phone: String) {
+
+        validate(name, phone)
+
+    }
+
+    fun delete(name: String, phone: String){
+
+        validateDelete(name, phone)
+    }
+}
