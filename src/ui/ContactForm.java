533cad82704e42d8aaee5841b574f770aad0db84
vitinho <victorsilva428@gmail.com>
2021-12-30 13:39:43 -0300

    Foi Listado

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,5 +1,7 @@
 package ui;
 
+import business.ContactBusiness;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,6 +15,8 @@ public class ContactForm extends JFrame {
     private JButton buttonSave;
     private JButton buttonCancele;
 
+    private ContactBusiness mContactBusiness;
+
     public ContactForm() {
         setContentPane(rootPanel);
         setSize(500, 250);
@@ -21,7 +25,11 @@ public class ContactForm extends JFrame {
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        mContactBusiness = new ContactBusiness();
+
         setListeners();
+
+
     }
 
     private void setListeners(){
@@ -36,7 +44,16 @@ public class ContactForm extends JFrame {
         buttonSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //coloca depois
+               try {
+                   String name =  textName.getText();
+                   String phone = textPhone.getText();
+
+                   mContactBusiness.save(name, phone);
+                   new MainForm();
+                   dispose();
+               }catch (Exception excp){
+                    JOptionPane.showMessageDialog(new JFrame(), excp.getMessage());
+               }
             }
         });
     }
